8620971604c50bcc9edcca56a9c5b73783fdf043
Schuyler Maclay <schuylermaclay@gmail.com>
2016-03-04 16:03:13 -0500

    oo

--- app.js
+++ app.js
@@ -1,31 +1,67 @@
 (function() {
   'use strict';
 
+  var CarouselController = function () {
+    this.currentBoxId = 1;
+    this.currentBox = document.getElementById("box" + this.currentBoxId);
+    this.leftButton = document.getElementById("left-button");
+    this.rightButton = document.getElementById("right-button");
+    this.boxes = document.getElementsByClassName("box");
+  };
 
-  var leftButton = document.getElementById("left-button");
-  leftButton.addEventListener("click", moveLeft);
-  var rightButton = document.getElementById("right-button");
-  rightButton.addEventListener("click", rightButton);
+  CarouselController.prototype.init = function() {
+    this.addButtonListeners();
+    for (var i = 0; i < this.boxes.length; i++) {
+      this.boxes[i].style.transform = 'translate(0px)';
+      this.boxes[i].style.webkitTransform = 'translate(0px)';
+    }
+  };
 
-  var boxes = document.getElementsByClassName("box");
-  for (var i = 0; i < boxes.length; i++) {
-    // console.log(boxes[i].style);
-    boxes[i].style.transform = 'translate(0px)';
-    boxes[i].style.webkitTransform = 'translate(0px)';
-  }
+  CarouselController.prototype.addButtonListeners = function () {
+    // console.log(this.boxes);
+    this.leftButton.addEventListener("click", this.moveLeft.bind(this));
+    this.rightButton.addEventListener("click", this.moveRight.bind(this));
+    // this.rightButton.addEventListener("click", this.moveRight);
+  };
 
+  CarouselController.prototype.moveLeft = function () {
+    this.currentBox = document.getElementById("box" + this.currentBoxId);
+    console.log(this.currentBox);
+    var boxes = this.boxes;
+    var position = ((this.currentBoxId * 600));
+    this.currentBoxId = (this.currentBoxId + 1);
+    console.log(this.currentBoxId);
+    for (var i = 0; i < boxes.length; i++) {
+      if (this.currentBoxId === 3) {
+        var nextBox = document.getElementById("box" + (1));
+        document.getElementById("carousel").appendChild(nextBox);
+        console.log("lastbox:", nextBox);
+        // nextBox.className = "jump";
+        nextBox.transform ='translate('+ -2*600 +'px)';
+        // nextBox.className = "box";
+        console.log("lastbox:reset:", nextBox);
+      }
+      boxes[i].style.transform = 'translate('+ position +'px)';
+      boxes[i].style.webkitTransform = 'translate('+ position +'px)';
+    }
+  };
+
+  CarouselController.prototype.moveRight = function () {
+    this.currentBox = document.getElementById("box" + this.currentBoxId);
+    // console.log(this.currentBox);
+    var boxes = this.boxes;
+    var position = (this.currentBoxId-2) * 600 + 'px';
+    this.currentBoxId = (this.currentBoxId - 1);
+    console.log(this.currentBoxId);
+    for (var i = 0; i < boxes.length; i++) {
+
+      boxes[i].style.transform = 'translate('+ position +')';
+      boxes[i].style.webkitTransform = 'translate('+ position +')';
+    }
+    // var nextBox = document.getElementById("box" + this.currentBoxId+1);
+    // nextBox.style.position = 3*600+'px';
+  };
+
+  var carouselController = new CarouselController();
+  carouselController.init();
 }());
-
-function moveLeft() {
-  var boxes = document.getElementsByClassName("box");
-  var position = '600px';
-  // transform: (20px, 20px)
-  console.log(boxes);
-
-  for (var i = 0; i < boxes.length; i++) {
-    // console.log(boxes[i].style);
-    boxes[i].style.transform = 'translate('+ position +')';
-    boxes[i].style.webkitTransform = 'translate('+ position +')';
-  }
-
-}
